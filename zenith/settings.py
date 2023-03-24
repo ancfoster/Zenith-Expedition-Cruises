569432c781ee93108f537d246241a46e40303275
ancfoster <a.foster@outlook.com>
2023-03-24 10:56:55 +0000

    Add CountryField library update settings.py

--- zenith/settings.py
+++ zenith/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'allauth',
+    'django_countries',
     'allauth.account',
     'allauth.socialaccount',
     'cruises',
