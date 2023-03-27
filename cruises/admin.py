07c305dd1584833999da07ed8861120dd79787fc
ancfoster <a.foster@outlook.com>
2023-03-27 12:01:57 +0000

    Register destinations in Django admin

--- cruises/admin.py
+++ cruises/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Destination, Ships, SuiteCategories, Suites, Tag, Cruises, Fares, Movements, Tickets, Bookings, Guests
+
 # Register your models here.
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country',)
+    ordering = ('name',)
+    fields = ('name', 'country', 'continent', 'description', 'image', 'latitude', 'longitude')
