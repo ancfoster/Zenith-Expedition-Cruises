de7e021957457ed140c8dc457352ab899cb8f5cc
ancfoster <a.foster@outlook.com>
2023-03-31 09:49:01 +0000

    Create admin.js & generated Tailwind style.css

---
+++ static/js/admin.js
@@ -0,0 +1,16 @@
+const burgerCont = document.getElementById('burgerCont');
+const burgerIcon = document.getElementById('burgerIcon');
+const adminNav = document.getElementById('adminNav');
+let menuStatus = 0;
+
+burgerCont.addEventListener("click", () => {
+    if (menuStatus === 0) {
+        burgerIcon.innerHTML = 'close';
+        menuStatus += 1;
+        adminNav.style.transform = 'translateX(0px)';
+    } else {
+        burgerIcon.innerHTML = 'menu';
+        menuStatus -= 1;
+        adminNav.style.transform = 'translateX(-100%)';
+    }
+});
