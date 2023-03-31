f1fe0bf92c17874b48f1a451c21f0146c1561313
ancfoster <a.foster@outlook.com>
2023-03-31 17:11:32 +0000

    Install widget_tweaks update settings requirements

--- zenith/settings.py
+++ zenith/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'widget_tweaks',
     'allauth',
     'storages',
     'django_countries',
