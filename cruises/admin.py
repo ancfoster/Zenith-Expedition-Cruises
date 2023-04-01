7f03e7b7bb71c7ec4a5abaa080baadfc8088f186
ancfoster <a.foster@outlook.com>
2023-04-01 13:55:19 +0000

    Update Destination model admin, show slug field

--- cruises/admin.py
+++ cruises/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from .models import Destination, Ships, SuiteCategories, Suites, Tag, Cruises, Fares, Movements, Tickets, Bookings, Guests
 
 # Register your models here.
+
+#Destination model
 @admin.register(Destination)
 class DestinationAdmin(admin.ModelAdmin):
     list_display = ('name', 'country',)
     ordering = ('name',)
-    fields = ('name', 'country', 'continent', 'description', 'image', 'latitude', 'longitude')
+    fields = ('name', 'country', 'continent', 'description', 'image', 'latitude', 'longitude', 'slug',)
+
+
+#Ships model
+@admin.register(Ships)
+class ShipsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ship_image')
+    fields = ('name', 'total_suites', 'info_page', 'ship_image')
