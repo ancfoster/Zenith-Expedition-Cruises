4c9d475f69e77e7792ee970f1f0ddb619683caa4
ancfoster <a.foster@outlook.com>
2023-04-02 10:41:29 +0000

    Add JS form field length count to destination form

---
+++ static/js/new_destination.js
@@ -0,0 +1,8 @@
+// Init variables
+const DescriptionField = document.getElementById('id_description');
+const DescriptionCount = document.getElementById('description_count')
+
+DescriptionField.addEventListener('input', countLength);
+function countLength(e) {
+   DescriptionCount.textContent = `${e.target.value.length} / 700`;
+}
