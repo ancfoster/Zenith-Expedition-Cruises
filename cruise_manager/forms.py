2838b8b613b1d571120a9e8c308a2a61537e20cc
ancfoster <a.foster@outlook.com>
2023-04-07 09:41:36 +0000

    Update new cruise form, create other field form

--- cruise_manager/forms.py
+++ cruise_manager/forms.py
@@ -20,10 +20,45 @@ class NewTagForm(forms.ModelForm):
         fields = ('name',)
 
 
-class NewCruiseForm(forms.ModelForm):
+class NewCruiseModelForm(forms.ModelForm):
     class Meta:
         model = Cruises
         widgets = {
             'tags' : forms.CheckboxSelectMultiple(),
+            'results_image': forms.ClearableFileInput(attrs={'accept': 'image/*'}),
+            'map_image': forms.ClearableFileInput(attrs={'accept': 'image/*'}),
+            'listing_image': forms.ClearableFileInput(attrs={'accept': 'image/*'}),
         }
-        fields = ('name', 'ship', 'duration', 'start_date', 'description', 'results_image', 'listing_image', 'map_image', 'bookable', 'tags')
+        fields = ('name', 'ship', 'duration', 'start_date', 'description',
+        'results_image', 'listing_image', 'map_image', 'bookable', 'tags',)
+
+
+class NewCruiseOtherFields(forms.Form):
+
+    #Fares for each category
+    verandah_suite_fare = forms.DecimalField(
+    max_digits=12,
+    decimal_places=2,
+    widget=forms.NumberInput(attrs={'class': 'form-control'}))
+
+    deluxe_verandah_suite_fare = forms.DecimalField(
+    max_digits=12,
+    decimal_places=2,
+    widget=forms.NumberInput(attrs={'class': 'form-control'}))
+
+    spa_suite_fare = forms.DecimalField(
+    max_digits=12,
+    decimal_places=2,
+    widget=forms.NumberInput(attrs={'class': 'form-control'}))
+
+
+class NewCruiseForm(forms.Form):
+    '''
+    This form combines the model form fields and
+    the non-model form fiels into a single form.
+    This form is the one that is used in the view.
+    '''
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.update(NewCruiseModelForm().fields)
+        self.fields.update(NewCruiseOtherFields().fields)
