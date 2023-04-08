96648b9503d4216485fc37187c9eab65c058e110
ancfoster <a.foster@outlook.com>
2023-04-08 10:57:02 +0000

    Calculate cruise end date, add code comments

--- static/js/new_cruise.js
+++ static/js/new_cruise.js
@@ -1,28 +1,46 @@
+/* jshint esversion: 11 */
+// Init variables
 const durationMinus =  document.getElementById('duration-minus');
 const durationPlus =  document.getElementById('duration-plus');
 const durationField = document.getElementById('id_duration');
 const durationSpan = document.getElementById('duration-span');
 const cruiseEndSpan = document.getElementById('cruise-end');
 const cruiseStartField = document.getElementById('id_start_date');
+const cruiseEndField = document.getElementById('id_end_date');
+let cruiseStartDate = new Date();
+let cruiseEndDate = new Date();
+let duration;
 
-let cruiseStartDate
-let cruiseEndDate
-let duration
-
-window.onload = loadValues();
+// Extract hidden field values
+window.onload = loadValues;
 
 function loadValues() {
     duration = parseInt(durationField.value);
     durationSpan.innerText = duration;
-    cruiseStartDate = Date(cruiseStartField.value);
-    cruiseEndDate.setDate(cruiseEndDate + duration);
-    cruiseEndSpan.innerText = cruiseEndDate
+    cruiseStartDate = new Date(cruiseStartField.value);
+    calculateEndDate();
 }
 
+// Calculates the end date of the cruise using start date and duration
+function calculateEndDate() {
+    cruiseEndDate.setDate(cruiseStartDate.getDate() + duration);
+    let cruiseEndDateDay = `${cruiseEndDate.getDate().toString().padStart(2, '0')}`;
+    let cruiseEndDateMonth = `${(cruiseEndDate.getMonth() + 1).toString().padStart(2, '0')}`;
+    let cruiseEndDateYear = `${cruiseEndDate.getFullYear().toString()}`;
+    // Displayed to user in dd-mm-yyyy format
+    cruiseEndSpan.innerText = `${cruiseEndDateDay}-${cruiseEndDateMonth}-${cruiseEndDateYear}`;
+    // This value is returned to backend in yyyy-mm-dd format
+    cruiseEndField.value = `${cruiseEndDateYear}-${cruiseEndDateMonth}-${cruiseEndDateDay}`;
+}
+
+
+// When the user changes the start date in the picker update hidden field value, recalculate end date
 cruiseStartField.addEventListener('change', () => {
-    cruiseStartDate = Date(cruiseStartField.value);
+    cruiseStartDate = new Date(cruiseStartField.value);
+    calculateEndDate();
 })
 
+// These two functions respond to the + - buttons being pressed then recalculate the cruise end date
 durationMinus.addEventListener('click', () => {
     if (duration == 2) {
         alert('Cruise duration cannot be less than two days')
@@ -31,6 +49,7 @@ durationMinus.addEventListener('click', () => {
         duration -= 1;
         durationField.value = duration;
         durationSpan.innerText = duration;
+        calculateEndDate();
     }
 })
 durationPlus.addEventListener('click', () => {
@@ -41,5 +60,6 @@ durationPlus.addEventListener('click', () => {
         duration += 1;
         durationField.value = duration;
         durationSpan.innerText = duration;
+        calculateEndDate();
     }
 })
