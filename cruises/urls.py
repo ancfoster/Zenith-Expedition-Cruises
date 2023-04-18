2f923b3d1e6cc232c4218ae6457b4948253d556f
ancfoster <a.foster@outlook.com>
2023-04-18 12:36:10 +0000

    Create cruise detail view and url

--- cruises/urls.py
+++ cruises/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    
+    path('cruises/<slug>/', views.CruiseDetail, name='cruise_detail'),
 ]
--- cruises/views.py
+++ cruises/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from cruises.models import Destination, Ships, SuiteCategories, Suites, Tag, Cruises, Fares, Movements, Tickets, Bookings, Guests
+from django.db.models import Count, Case, When, BooleanField
+from django.contrib.auth.decorators import login_required
+from django.db.models import F
+from django.views import generic, View
 
-# Create your views here.
+def CruiseDetail(request, slug):
+    '''
+    Displays the detail page of specific cruise
+    '''
+    cruise = get_object_or_404(Cruises, slug=slug)
+    movements = Movements.objects.filter(cruise=cruise)
+    fares = Fares.objects.filter(cruise=cruise)
+    suite_cats = SuiteCategories.objects.all()
+    verandah_tickets_available = Tickets.objects.filter(cruise=cruise, booked=False, suite__category__id=1).count()
+
+    context = {
+        'cruise' : cruise,
+        'movements': movements,
+        'suite_cats': suite_cats,
+        'verandah_tickets_available' : verandah_tickets_available,
+        'fares': fares,
+    }
+    return render(request, 'cruises/detail_view.html', context)
