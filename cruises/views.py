ab228454bef105226433030534bb84f284ae428a
ancfoster <a.foster@outlook.com>
2023-04-20 22:24:06 +0000

    Update CruiseDetail, context

--- cruises/views.py
+++ cruises/views.py
@@ -12,14 +12,10 @@ def CruiseDetail(request, slug):
     cruise = get_object_or_404(Cruises, slug=slug)
     movements = Movements.objects.filter(cruise=cruise)
     fares = Fares.objects.filter(cruise=cruise)
-    suite_cats = SuiteCategories.objects.all()
-    verandah_tickets_available = Tickets.objects.filter(cruise=cruise, booked=False, suite__category__id=1).count()
 
     context = {
         'cruise' : cruise,
         'movements': movements,
-        'suite_cats': suite_cats,
-        'verandah_tickets_available' : verandah_tickets_available,
         'fares': fares,
     }
     return render(request, 'cruises/detail_view.html', context)
