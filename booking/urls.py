1c31f940235aa8e55d02fd3c308cce91303ca025
ancfoster <a.foster@outlook.com>
2023-04-21 13:10:56 +0000

    Add suite category url

--- booking/urls.py
+++ booking/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('book/<slug>/', views.PassengerNumber, name='new_booking'),
+    path('booking/suite-category/', views.SuiteCategory, name='booking_suite_category'),
 ]
