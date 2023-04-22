10782354cad6714f52207800f30396aa97e3b5b7
ancfoster <a.foster@outlook.com>
2023-04-22 17:09:06 +0000

    Add UpdateTotalPrice function

--- static/js/book.js
+++ static/js/book.js
@@ -4,40 +4,81 @@ const numberField = document.getElementById('id_number_guests');
 const guestNumberSpan = document.getElementById('guest_span_count');
 const plus = document.getElementById('plus');
 const minus = document.getElementById('minus');
-let count = 0;
+let numberGuests = 0;
 
-window.onload = setCount;
+// Price variables
+const totalPriceVerandah = document.getElementById('total_price_verandah');
+const totalPriceDeluxe = document.getElementById('total_price_deluxe');
+const totalPriceSpa = document.getElementById('total_price_spa');
+const totalPriceOwner = document.getElementById('total_price_owner');
 
-function setCount() {
-    count = 2;
-    numberField.value = count
+
+
+window.onload = set;
+
+function set() {
+    numberGuests = 2;
+    numberField.value = numberGuests
+    updateTotalPrice()
+}
+// Updates total price based on number of passengers
+function updateTotalPrice() {
+    // 1 guest = 25% discount
+    // 3 guests = 50% surcharge
+    let multiplier = 0
+    switch (numberGuests){
+        case 1:
+            multiplier = 0.75;
+            break;
+        case 2:
+            multiplier = 1;
+            break;
+        case 3:
+            multiplier = 1.5;
+            break;
+    }
+    // Checks if price span exists, if so update the price based on multipler
+    if (totalPriceVerandah) {
+        totalPriceVerandah.innerText = (verandah_price * multiplier).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    }
+    if (totalPriceDeluxe) {
+        totalPriceVerandah.innerText = (deluxe_price * multiplier).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    }
+    if (totalPriceSpa) {
+        totalPriceVerandah.innerText = (spa_price * multiplier).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    }
+    if (totalPriceOwner) {
+        totalPriceVerandah.innerText = (owner_price * multiplier).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    }
 }
 
 // Controls number of passengers increment buttons
 plus.addEventListener('click', () => {
-    if (count < 3) {
-        count++
-        numberField.value = count
-        guestNumberSpan.innerText = count;
-        if (count === 3) {
+    if (numberGuests < 3) {
+        numberGuests++
+        numberField.value = numberGuests
+        guestNumberSpan.innerText = numberGuests;
+        if (numberGuests === 3) {
             plus.style.cursor = "not-allowed";
         }
-        if ( count > 1) {
+        if ( numberGuests > 1) {
             minus.style.cursor = "pointer";
         }
+        updateTotalPrice();
     }
 })
 // Minus number of guests
 minus.addEventListener('click', () => {
-    if (count > 1) {
-        count--
-        numberField.value = count
-        guestNumberSpan.innerText = count;
-        if (count === 1) {
+    if (numberGuests > 1) {
+        numberGuests--
+        numberField.value = numberGuests
+        guestNumberSpan.innerText = numberGuests;
+        if (numberGuests === 1) {
             minus.style.cursor = "not-allowed";
         }
-        if ( count < 3) {
+        if ( numberGuests < 3) {
             plus.style.cursor = "pointer";
         }
+        updateTotalPrice();
     }
 })
