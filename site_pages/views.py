c7c56188f95103482ca068225ee3ff63faf27876
ancfoster <a.foster@outlook.com>
2023-04-25 18:35:29 +0000

    Create empty site pages, update urls and views

--- site_pages/views.py
+++ site_pages/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from cruises.models import Destination, Ships, SuiteCategories, Suites, Tag, Cruises, Fares, Movements, Tickets, Bookings, Guests
+from django.db.models import Count, Case, When, BooleanField
+from django.contrib.auth.decorators import login_required
+from django.db.models import F
+from django.views import generic, View
+
 
 # Create your views here.
+def HomePage(request):
+    '''
+    Displays the home page
+    '''
+    context = {
+
+    }
+    return render(request, 'site_pages/index.html', context)
