474b68c2b830728b8f70a4d23b3b49a2482a57d1
ancfoster <a.foster@outlook.com>
2023-04-27 21:25:08 +0000

    Create a robots.txt and register it in urlpatterns

--- zenith/urls.py
+++ zenith/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 from site_pages.views import handler404, handler500
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import CruiseSitemap
@@ -33,6 +34,7 @@ urlpatterns = [
     path('', include("booking.urls"), name="booking_urls"),
     path('accounts/', include('allauth.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ]
 
 
