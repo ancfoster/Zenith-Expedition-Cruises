46cf848355f5040c04f9a7ace343cc7ac3d6a8c8
ancfoster <a.foster@outlook.com>
2023-04-27 03:15:51 +0000

    Add descriptions and media to the experience page

--- site_pages/urls.py
+++ site_pages/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePage, name='home'),
+    path('', views.Experience, name='experience'),
+    path('', views.Contact, name='contact'),
 ]
--- site_pages/views.py
+++ site_pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.conf.urls import handler404, handler500
-from cruises.models import Destination, Ships, SuiteCategories, Suites, Tag, Cruises, Fares, Movements, Tickets, Bookings
+from cruises.models import Destination
 from django.db.models import Count, Case, When, BooleanField
 from django.contrib.auth.decorators import login_required
 from django.db.models import F
@@ -13,6 +13,23 @@ def HomePage(request):
     Displays the home page
     '''
     context = {
-
     }
     return render(request, 'site_pages/index.html', context)
+
+
+def Experience(request):
+    '''
+    Displays the contact page page
+    '''
+    context = {
+    }
+    return render(request, 'site_pages/experience.html', context)
+
+
+def Contact(request):
+    '''
+    Displays the contact page page
+    '''
+    context = {
+    }
+    return render(request, 'site_pages/contact.html', context)
