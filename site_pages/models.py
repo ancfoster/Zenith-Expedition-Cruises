47d664ed411ab6573a848718852b604cb7bf0925
ancfoster <a.foster@outlook.com>
2023-04-27 03:23:05 +0000

    Create enquiry model

--- site_pages/models.py
+++ site_pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Enquiry(models.Model):
+    '''
+    Model for users to send contact messages
+    '''
+    name = models.CharField(max_length=30, verbose_name='Name')
+    email = models.EmailField(max_length=60, verbose_name='Email')
+    phone = models.CharField(max_length=20, verbose_name='Phone')
+    message = models.TextField(max_length=900, verbose_name='Message')
+
+
