5af8442983e47dba2bc66005ec27f2dde75ffd4c
ancfoster <a.foster@outlook.com>
2023-04-27 03:53:31 +0000

    Register Enquiry model in Django admin

--- site_pages/admin.py
+++ site_pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Enquiry
 
 # Register your models here.
+#Enquiry model
+@admin.register(Enquiry)
+class EquiryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sent', 'responded_to')
+    fields = ('name', 'sent', 'phone', 'email', 'message')
