c0d6093b62617e75609fd132a52d16e7f26d7a08
ancfoster <a.foster@outlook.com>
2023-04-28 00:48:39 +0000

    Edit BookingForm to pass pep8

--- booking/forms.py
+++ booking/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cruises.models import Destination, Tag, Cruises, Suites, SuiteCategories, Bookings
+from cruises.models import Destination, Tag, Cruises, Suites, SuiteCategories, Bookings  # noqa
 from datetime import date, timedelta
 
 
@@ -8,19 +8,18 @@ class BookingForm(forms.Form):
     Form for a new cruise booking
     '''
     number_guests = forms.IntegerField(
-    min_value=1,
-    max_value=3,
-    widget=forms.HiddenInput()
+        min_value=1,
+        max_value=3,
+        widget=forms.HiddenInput()
     )
     selected_category = forms.IntegerField(
-    min_value=1,
-    max_value=4,
-    widget=forms.HiddenInput()
+        min_value=1,
+        max_value=4,
+        widget=forms.HiddenInput()
     )
     selected_suite = forms.IntegerField(
-    widget=forms.HiddenInput()
+        widget=forms.HiddenInput()
     )
     guest_information = forms.CharField(
-    widget=forms.HiddenInput()
+        widget=forms.HiddenInput()
     )
-
