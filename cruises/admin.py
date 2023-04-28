2c80e0a385be9169525af004f839cee97a7bf04f
ancfoster <a.foster@outlook.com>
2023-04-28 02:31:33 +0000

    Update to pass PEP8 validation

--- cruises/admin.py
+++ cruises/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 
-from .models import Destination, Ships, SuiteCategories, Suites, Tag, Cruises, Fares, Movements, Tickets, Bookings
+from .models import Destination, Ships, SuiteCategories, Suites, Tag, Cruises, Fares, Movements, Tickets, Bookings  # noqa
 
-# Register your models here.
 
-#Destination model
+# Destination model
 @admin.register(Destination)
 class DestinationAdmin(admin.ModelAdmin):
     list_display = ('name', 'country',)
     ordering = ('name',)
-    fields = ('name', 'country', 'continent', 'description', 'image', 'latitude', 'longitude', 'slug',)
+    fields = ('name', 'country', 'continent', 'description',
+              'image', 'latitude', 'longitude', 'slug',)
 
 
-#Ships model
+# Ships model
 @admin.register(Ships)
 class ShipsAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'ship_image')
@@ -20,14 +20,15 @@ class ShipsAdmin(admin.ModelAdmin):
     readonly_fields = fields
 
 
-#Suite category model
+# Suite category model
 @admin.register(SuiteCategories)
 class CategoriesAdmin(admin.ModelAdmin):
-    list_display = ('id','name', 'description')
-    fields = ('name', 'description', 'sleeps', 'size', 'suite_image', 'suite_layout_image', 'suite_feature_list', 'category_deckplan')
+    list_display = ('id', 'name', 'description')
+    fields = ('name', 'description', 'sleeps', 'size', 'suite_image',
+              'suite_layout_image', 'suite_feature_list', 'category_deckplan')
 
 
-#Suites model 
+# Suites model
 @admin.register(Suites)
 class SuitesAdmin(admin.ModelAdmin):
     list_display = ('suite_num_name', 'ship', 'category')
@@ -35,10 +36,10 @@ class SuitesAdmin(admin.ModelAdmin):
     readonly_fields = fields
 
     def has_delete_permission(self, request, obj=None):
-            return False
+        return False
 
 
-#Tag model
+# Tag model
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -48,12 +49,14 @@ class TagAdmin(admin.ModelAdmin):
 # Cruises model
 @admin.register(Cruises)
 class CruisesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'ship', 'start_date', 'end_date', 'bookable', 'slug', 'port_number',)
-    fields = ('name', 'ship', 'created_on', 'duration', 'start_date', 'end_date', 'bookable', 'slug', 'description', 'results_image', 'listing_image', 'map_image', 'tags',)
+    list_display = ('name', 'ship', 'start_date', 'end_date',
+                    'bookable', 'slug', 'port_number',)
+    fields = ('name', 'ship', 'created_on', 'duration', 'start_date', 'end_date', 'bookable',  # noqa
+              'slug', 'description', 'results_image', 'listing_image', 'map_image', 'tags',)  # noqa
     readonly_fields = fields
 
 
-#Tickets model
+# Tickets model
 @admin.register(Tickets)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('ticket_ref', 'cruise', 'suite', 'booked', 'created_on',)
@@ -61,7 +64,7 @@ class TicketAdmin(admin.ModelAdmin):
     readonly_fields = ('ticket_ref', 'ship', 'cruise', 'suite', 'created_on',)
 
 
-#Movement model
+# Movement model
 @admin.register(Movements)
 class Movements(admin.ModelAdmin):
     list_display = ('cruise', 'date', 'type', 'destination', 'order',)
@@ -69,16 +72,18 @@ class Movements(admin.ModelAdmin):
     readonly_fields = fields
 
 
-#Fares model
+# Fares model
 @admin.register(Fares)
 class Fares(admin.ModelAdmin):
     list_display = ('cruise', 'suite_category', 'price')
     fields = ('cruise', 'suite_category', 'price')
 
 
-#Bookings model
+# Bookings model
 @admin.register(Bookings)
 class Bookings(admin.ModelAdmin):
-    list_display = ('booking_ref', 'booked_by', 'booking_price', 'booked_on', 'ticket')
-    fields = ('booking_ref', 'booked_by', 'booking_price', 'booked_on', 'ticket')
+    list_display = ('booking_ref', 'booked_by',
+                    'booking_price', 'booked_on', 'ticket')
+    fields = ('booking_ref', 'booked_by',
+              'booking_price', 'booked_on', 'ticket')
     readonly_fields = fields
